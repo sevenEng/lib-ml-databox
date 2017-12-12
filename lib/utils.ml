e2989321688a8d4f415cfc0c18f7f67485e5657f
Qi Li <liqi0425@gmail.com>
2017-12-12 16:34:05 +0000

    support token cache

--- lib/utils.ml
+++ lib/utils.ml
@@ -3,6 +3,7 @@ open Lwt.Infix
 type arbiter = {
   arbiter_endpoint: Uri.t option;
   arbiter_token: string;
+  token_cache: (string * string * string, string) Hashtbl.t;
 }
 
 type databox_ctx = {
@@ -36,13 +37,19 @@ let databox_init () : databox_ctx =
       Some (Uri.of_string endp)
     with _ -> None in
   if arbiter_endpoint = None then
-    let arbiter = {arbiter_endpoint; arbiter_token = "";} in
-    {arbiter;
-     store_endpoint = "tcp://127.0.0.1:5555";
-     store_key = "vl6wu0A@XP?}Or/&BR#LSxn>A+}L)p44/W[wXL3<"}
+    let arbiter = {
+      arbiter_endpoint;
+      arbiter_token = "";
+      token_cache = Hashtbl.create 23; } in
+    { arbiter;
+      store_endpoint = "tcp://127.0.0.1:5555";
+      store_key = "vl6wu0A@XP?}Or/&BR#LSxn>A+}L)p44/W[wXL3<"}
   else
     let arbiter_token = arbiter_token () in
-    let arbiter = {arbiter_endpoint; arbiter_token;} in
+    let arbiter = {
+      arbiter_endpoint;
+      arbiter_token;
+      token_cache = Hashtbl.create 23; } in
     let store_endpoint = store_endpoint () in
     let store_key = store_key () in
     {arbiter; store_endpoint; store_key}
@@ -61,21 +68,39 @@ let body_of_json json =
 
 let headers = Cohttp.Header.init_with "Content-Type" "application/json"
 
+let check_cache t host path meth =
+  try Some (Hashtbl.find t.token_cache (host, path, meth))
+  with Not_found -> None
+
+let clear_entry t timeout host path meth =
+  Lwt_unix.sleep timeout >>= fun () ->
+  Hashtbl.remove t.token_cache (host, path, meth);
+  Lwt.return_unit
+
 let request_token t ~host ~path ~meth =
   let headers = Cohttp.Header.add headers "x-api-key" t.arbiter_token in
   match t.arbiter_endpoint with
   | None ->  Lwt.return ""
   | Some arbiter_endpoint ->
-    let uri = Uri.with_path arbiter_endpoint "/token" in
-    let body = `O [
-      "target", `String host;
-      "path", `String path;
-      "method", `String meth
-    ] |> body_of_json in
-    Client.post ~body ~headers uri >>= fun (resp, body) ->
-    if Cohttp.Response.status resp <> `OK
-    then Lwt.fail @@ Failure (Uri.to_string uri)
-    else Cohttp_lwt_body.to_string body
+    let hostname = Uri.of_string host |> Uri.host_with_default ~default:"" in
+    match check_cache t hostname path meth with
+    | Some cached -> Lwt.return cached
+    | None ->
+        let uri = Uri.with_path arbiter_endpoint "/token" in
+        let body = `O [
+          "target", `String hostname;
+          "path", `String path;
+          "method", `String meth
+        ] |> body_of_json in
+        Client.post ~body ~headers uri >>= fun (resp, body) ->
+        if Cohttp.Response.status resp <> `OK
+        then Lwt.fail @@ Failure (Uri.to_string uri)
+        else begin
+          Cohttp_lwt_body.to_string body >>= fun token ->
+          Hashtbl.add t.token_cache (hostname, path, meth) token;
+          Lwt.async (fun () -> clear_entry t 120. hostname path meth);
+          Lwt.return token
+        end
 
 let https_creds () =
   let cert = Fpath.add_seg secrets_dir "DATABOX.pem" in
